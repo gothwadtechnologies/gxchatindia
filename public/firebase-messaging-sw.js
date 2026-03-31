6318f5d6ae53ab92a83bd0b8174a024d17efcad4
Gothwad Technologies <pawangothwad@gmail.com>
2026-03-31 12:04:26 +0530

    Update public/firebase-messaging-sw.js via GTech AI Studio

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,13 +1,27 @@
 importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyAeY_ZPOqrdmnnzpKR1DhYQYGioiPCbvws",
-  authDomain: "gxchatindia.firebaseapp.com",
-  projectId: "gxchatindia",
-  messagingSenderId: "709776621586",
-  appId: "1:709776621586:web:39f0f3f49eb74dc37458cb"
-});
+const params = new URL(self.location).searchParams;
+const firebaseConfig = {
+  apiKey: params.get('apiKey'),
+  authDomain: params.get('authDomain'),
+  projectId: params.get('projectId'),
+  messagingSenderId: params.get('messagingSenderId'),
+  appId: params.get('appId')
+};
+
+if (firebaseConfig.apiKey) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  // Fallback for direct registration or if params are missing
+  firebase.initializeApp({
+    apiKey: "AIzaSyAeY_ZPOqrdmnnzpKR1DhYQYGioiPCbvws",
+    authDomain: "gxchatindia.firebaseapp.com",
+    projectId: "gxchatindia",
+    messagingSenderId: "709776621586",
+    appId: "1:709776621586:web:39f0f3f49eb74dc37458cb"
+  });
+}
 
 const messaging = firebase.messaging();
 
