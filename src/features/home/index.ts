2e96b6f1893c5a9ec7d6b84581e1b3f3c1b5eea5
Gothwad Technologies <pawangothwad@gmail.com>
2026-04-07 07:08:03 +0530

    Update src/features/home/index.ts via GTech AI Studio

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,3 +1,2 @@
-export { default as StatusTab } from './StatusTab';
 export { default as CreatePostScreen } from './CreatePostScreen';
 export { default as NotificationsScreen } from './NotificationsScreen';
